0f1b919a655b8b83f8fe0132f6415be5daa9cdd0
Dang <wheodnjs2@naver.com>
2025-11-04 23:38:16 +0900

    SSR 실습

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,40 @@
 // CSS Module
-import SearchableLayout from "@/components/searchable-layout";
-import style from "./index.module.css";
-import { ReactNode } from "react";
-import books from "@/mock/books.json";
-import BookItem from "@/components/book-item";
+import SearchableLayout from '@/components/searchable-layout';
+import style from './index.module.css';
+import { ReactNode, useEffect } from 'react';
+import books from '@/mock/books.json';
+import BookItem from '@/components/book-item';
+import { InferGetServerSidePropsType } from 'next';
 
-export default function Home() {
+export const getServerSideProps = () => {
+  // 불 가능 (브라우저에서만 사용할 수 있는 기능이라서)
+  // window.alert()
+
+  const data = 'hello';
+
+  // 터미널에서 만 보임
+  console.log('서버에서만 보여요');
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+export default function Home({
+  data,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  // 불 가능 (브라우저에서만 사용할 수 있는 기능이라서)
+  // window.alert()
+
+  // 가능
+  console.log(data);
+
+  useEffect(() => {
+    console.log('브라우저에서만 보여요');
+  }, []);
+
   return (
     <div className={style.container}>
       <section>
