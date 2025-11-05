99ea43aa9a5f0c8c18bc10c7ac5dbbed797e070a
Dang <wheodnjs2@naver.com>
2025-11-06 00:56:51 +0900

    검색 및 색 상세 패칭

--- src/lib/fetch-one-book.ts
+++ src/lib/fetch-one-book.ts
@@ -1,11 +1,9 @@
 import { BookData } from '@/types';
 
-export default async function fetchBooks(q?: string): Promise<BookData[]> {
-  let url = `http://localhost:12345/book`;
-
-  if (q) {
-    url += `/search?q=${q}`;
-  }
+export default async function fetchOneBook(
+  id: number
+): Promise<BookData | null> {
+  const url = `http://localhost:12345/book/${id}`;
 
   try {
     const response = await fetch(url);
@@ -16,6 +14,6 @@ export default async function fetchBooks(q?: string): Promise<BookData[]> {
     return await response.json();
   } catch (err) {
     console.error(err);
-    return [];
+    return null;
   }
 }
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,10 +1,30 @@
-import SearchableLayout from "@/components/searchable-layout";
-import { useRouter } from "next/router";
-import { ReactNode } from "react";
-import books from "@/mock/books.json";
-import BookItem from "@/components/book-item";
+import SearchableLayout from '@/components/searchable-layout';
+import { useRouter } from 'next/router';
+import { ReactNode } from 'react';
+import books from '@/mock/books.json';
+import BookItem from '@/components/book-item';
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
+import fetchBooks from '@/lib/fetch-books';
 
-export default function Page() {
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  // 현재 브라우저에서 받은 모든 요청에 대한 정보를 알 수 있음
+  // console.log(context);
+
+  const q = context.query.q;
+
+  // q가 undefined일 수 있으니 단언 해준다.
+  const books = await fetchBooks(q as string);
+
+  return {
+    props: { books },
+  };
+};
+
+export default function Page({
+  books,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div>
       {books.map((book) => (
