c147f713266d7b3b41db5e47fcc496bd92071501
Dang <wheodnjs2@naver.com>
2025-11-06 19:16:05 +0900

    SSG 동적 경로에 적용

--- src/pages/book/[id].tsx
+++ src/pages/book/[id].tsx
@@ -1,25 +1,26 @@
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
+import { GetStaticPropsContext, InferGetStaticPropsType } from 'next';
 import style from './[id].module.css';
 import fetchOneBook from '@/lib/fetch-one-book';
 
-const mockData = {
-  id: 1,
-  title: '한 입 크기로 잘라 먹는 리액트',
-  subTitle: '자바스크립트 기초부터 애플리케이션 배포까지',
-  description:
-    '자바스크립트 기초부터 애플리케이션 배포까지\n처음 시작하기 딱 좋은 리액트 입문서\n\n이 책은 웹 개발에서 가장 많이 사용하는 프레임워크인 리액트 사용 방법을 소개합니다. 인프런, 유데미에서 5000여 명이 수강한 베스트 강좌를 책으로 엮었습니다. 프런트엔드 개발을 희망하는 사람들을 위해 리액트의 기본을 익히고 다양한 앱을 구현하는 데 부족함이 없도록 만들었습니다. \n\n자바스크립트 기초 지식이 부족해 리액트 공부를 망설이는 분, 프런트엔드 개발을 희망하는 취준생으로 리액트가 처음인 분, 퍼블리셔나 백엔드에서 프런트엔드로 직군 전환을 꾀하거나 업무상 리액트가 필요한 분, 뷰, 스벨트 등 다른 프레임워크를 쓰고 있는데, 실용적인 리액트를 배우고 싶은 분, 신입 개발자이지만 자바스크립트나 리액트 기초가 부족한 분에게 유용할 것입니다.',
-  author: '이정환',
-  publisher: '프로그래밍인사이트',
-  coverImgUrl:
-    'https://shopping-phinf.pstatic.net/main_3888828/38888282618.20230913071643.jpg',
+// 다이나믹한 페이지라서 getStaticPaths를 해주어야 한다.
+export const getStaticPaths = () => {
+  return {
+    paths: [
+      // '1'는 반드시 문자열로 입력해야 한다.
+      { params: { id: '1' } },
+      { params: { id: '2' } },
+      { params: { id: '3' } },
+    ],
+
+    // 만약 id:'4' 같이 존재하지 않는 것들이 들어올때 대비책으로 넣어둠
+    // false는 paths에 작성하지 않은 값이 들어오면
+    // 무조건 not found 페이지로 이동시킴
+    fallback: false,
+  };
 };
 
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext
-) => {
-  // !로 단언해준다.
+export const getStaticProps = async (context: GetStaticPropsContext) => {
   const id = context.params!.id;
-
   const book = await fetchOneBook(Number(id));
 
   return {
@@ -29,7 +30,7 @@ export const getServerSideProps = async (
 
 export default function Page({
   book,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   if (!book) return '문제가 발생했습니다. 다시 시도하세요';
 
   const { id, title, subTitle, description, author, publisher, coverImgUrl } =
